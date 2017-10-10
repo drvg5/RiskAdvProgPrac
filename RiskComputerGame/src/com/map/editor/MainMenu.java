ebd7af013f24f085d144d1b3a40eb5c05868d2e3
vkhashyap <32470077+vkhashyap@users.noreply.github.com>
2017-10-09 21:54:54 -0400

    First Commit

---
+++ RiskComputerGame/src/com/map/editor/MainMenu.java
@@ -0,0 +1,52 @@
+package com.map.editor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainMenu {
+	public static void main(String args[]) {
+
+		JFrame jframeMain = new JFrame("Risk Game");
+		JButton jbuttonNewGame = new JButton("New Game");
+		JButton jbuttonMapEditor = new JButton("Map Editor");
+		JButton jbuttonRules = new JButton("Rules");
+		jbuttonNewGame.setBounds(200, 150, 200, 30);
+		jbuttonMapEditor.setBounds(200, 200, 200, 30);
+		jbuttonRules.setBounds(200, 250, 200, 30);
+		jframeMain.setLayout(null);
+		jframeMain.add(jbuttonNewGame);
+		jframeMain.add(jbuttonMapEditor);
+		jframeMain.add(jbuttonRules);
+		jframeMain.setSize(600, 400);
+		jframeMain.setLocationRelativeTo(null);
+		jframeMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframeMain.setVisible(true);
+
+		jbuttonNewGame.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
+		jbuttonMapEditor.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MapEditorMenu.mapEditorNavigation();
+			}
+		});
+
+		jbuttonRules.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+	}
+
+}
---
+++ RiskComputerGame/src/com/map/editor/MapEditorMenu.java
@@ -0,0 +1,55 @@
+package com.map.editor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MapEditorMenu {
+
+	public static void mapEditorNavigation() {
+		JFrame jframeMapEditor = new JFrame("Map Editor");
+		JButton jbuttonCreateMap = new JButton("Create Map");
+		JButton jbuttonEditMap = new JButton("Edit Map");
+		JButton jbuttonMainMenu = new JButton("Main Menu");
+		//MainMenu objMainMenu = new MainMenu();
+		jbuttonCreateMap.setBounds(200, 150, 200, 30);
+		jbuttonEditMap.setBounds(200, 200, 200, 30);
+		jbuttonMainMenu.setBounds(200, 250, 200, 30);
+		jframeMapEditor.setLayout(null);
+		jframeMapEditor.add(jbuttonCreateMap);
+		jframeMapEditor.add(jbuttonEditMap);
+		jframeMapEditor.add(jbuttonMainMenu);
+		jframeMapEditor.setSize(600, 400);
+		jframeMapEditor.setLocationRelativeTo(null);
+		jframeMapEditor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframeMapEditor.setVisible(true);
+
+		jbuttonCreateMap.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
+		jbuttonEditMap.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		jbuttonMainMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MainMenu.main(null);
+			}
+		});
+		
+	}
+
+}
