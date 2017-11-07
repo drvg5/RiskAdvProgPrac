f37c57564d21d7770278bc2a398cc378ef490019
Navjot <Navjot@DESKTOP-Q1V7OLO.wireless.concordia.ca>
2017-11-07 01:19:11 -0500

    Added PlayerDominationUI Observer class

---
+++ RiskComputerGame/src/com/risk/ui/PlayerDominationUI.java
@@ -0,0 +1,18 @@
+package com.risk.ui;
+
+import java.util.*;
+
+public class PlayerDominationUI implements Observer {
+
+	public static HashMap<String,Float> playerDominationMap = new  HashMap<String,Float>();
+	
+	public void playerDominationView(){
+		
+		int player = 0  ;
+		System.out.println("Player " + player );
+	}
+	public void update(Observable obs, Object x){
+		
+		
+	}
+}
