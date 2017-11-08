6a505a2bd4652e5155ccae0fbabcb1bc1134706b
Navjot <Navjot@DESKTOP-Q1V7OLO.local>
2017-11-07 19:08:29 -0500

    Reinforcement Phase view added

--- RiskComputerGame/src/com/risk/ui/PhaseUI.java
+++ RiskComputerGame/src/com/risk/ui/PhaseUI.java
@@ -2,6 +2,8 @@ package com.risk.ui;
 
 import java.util.*;
 
+import com.risk.model.StartUpPhaseModel;
+
 public class PhaseUI implements Observer {
 
 	
@@ -9,8 +11,33 @@ public class PhaseUI implements Observer {
 		
 	}
 	
-	public void forticationView(){
+	public void forticationView(String player, HashMap<String,List<String>> territoryMap){
 		
+		for(String playerInfoKey : StartUpPhaseModel.playerInfo.keySet()){
+		
+		String[] playerInfoVal = playerInfoKey.split("-");
+		
+		if(player.equals(playerInfoVal[0]) || player == playerInfoVal[0]){
+			
+			System.out.println("\n\t--------------");
+			System.out.println("\t" + "Territory : " + playerInfoVal[1]);
+			
+			System.out.println("\t\t" + "Continent : " + playerInfoVal[2]);
+			
+			System.out.println("\t\t" + "Armies : " + StartUpPhaseModel.playerInfo.get(playerInfoKey));
+			
+			System.out.printf("\t\t"+"Adjacent Countries : ");
+			
+			List<String> adjacentList = territoryMap.get(playerInfoVal[2] + "," + playerInfoVal[1]);
+			
+			for(String adjacent : adjacentList){
+				System.out.printf(adjacent+"\t");
+			}
+			
+		}
+		
+	}
+
 	}
 	
 	public void attackView(){
