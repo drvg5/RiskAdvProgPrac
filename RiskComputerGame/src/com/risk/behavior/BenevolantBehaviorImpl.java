ea5ba93227732a25a03d43cec3adbde1064ef684
drvg5 <drvg5@DESKTOP-2HQ1VCH.wireless.concordia.ca>
2017-11-27 05:49:28 -0500

    strategy changes made

--- RiskComputerGame/src/com/risk/behavior/BenevolantBehaviorImpl.java
+++ RiskComputerGame/src/com/risk/behavior/BenevolantBehaviorImpl.java
@@ -13,113 +13,101 @@ import com.risk.model.StartUpPhaseModel;
 public class BenevolantBehaviorImpl extends Observable implements PlayerBehavior {
 
 	@Override
-	public void reinforce(String player){
-		
-				
+	public void reinforce(String player) {
+
 		ReinforcementPhaseModel obj = new ReinforcementPhaseModel();
 		obj.setMsgUI("total reinforcement print," + player);
 
 		setChanged();
-		
+
 		notifyObservers(this);
-		
 
 		int reinforcementArmies = ReinforcementPhaseModel.reinforcement.get(player);
-		
-		
+
 		int leastArmies = 0;
-		
-		//create a list of keys from playerInfo HashMap 
-		//for only the player concerned with the least armies
+
+		// create a list of keys from playerInfo HashMap
+		// for only the player concerned with the least armies
 		List<String> weakestTerritoryList = new ArrayList<String>();
-		
+
 		TreeSet<Integer> unitsSet = new TreeSet<Integer>();
-		
-		
-		//populate unitsSet
-		for(String playerInfoKey : StartUpPhaseModel.playerInfo.keySet()){
-			
-			String [] playerVals = playerInfoKey.split("-");
-			
-			if(playerVals[0].equals(player) || playerVals[0] == player){
-				
-					
-					unitsSet.add(StartUpPhaseModel.playerInfo.get(playerInfoKey));
-					
-				
-			}//end if(playerVals[0].equals(player) || playerVals[0] == player)
-			
-		}//end for(String playerInfoKey : StartUpPhaseModel.playerInfo.keySet())
-		
+
+		// populate unitsSet
+		for (String playerInfoKey : StartUpPhaseModel.playerInfo.keySet()) {
+
+			String[] playerVals = playerInfoKey.split("-");
+
+			if (playerVals[0].equals(player) || playerVals[0] == player) {
+
+				unitsSet.add(StartUpPhaseModel.playerInfo.get(playerInfoKey));
+
+			} // end if(playerVals[0].equals(player) || playerVals[0] == player)
+
+		} // end for(String playerInfoKey : StartUpPhaseModel.playerInfo.keySet())
+
 		leastArmies = unitsSet.first();
-		
-		
-		
-		//populate weakestTerritoryList
-		for(String playerInfoKey : StartUpPhaseModel.playerInfo.keySet()){
-			
-			String [] playerVals = playerInfoKey.split("-");
-			
-			if(playerVals[0].equals(player) || playerVals[0] == player){
-				
-				if(StartUpPhaseModel.playerInfo.get(playerInfoKey) == leastArmies){
-					
+
+		// populate weakestTerritoryList
+		for (String playerInfoKey : StartUpPhaseModel.playerInfo.keySet()) {
+
+			String[] playerVals = playerInfoKey.split("-");
+
+			if (playerVals[0].equals(player) || playerVals[0] == player) {
+
+				if (StartUpPhaseModel.playerInfo.get(playerInfoKey) == leastArmies) {
+
 					weakestTerritoryList.add(playerInfoKey);
-					
-				}//end if(StartUpPhaseModel.playerInfo.get(playerInfoKey) > highestArmies)
-				
-			}//end if(playerVals[0].equals(player) || playerVals[0] == player)
-			
-		}//end for(String playerInfoKey : StartUpPhaseModel.playerInfo.keySet()
-		
-		
+
+				} // end if(StartUpPhaseModel.playerInfo.get(playerInfoKey) > highestArmies)
+
+			} // end if(playerVals[0].equals(player) || playerVals[0] == player)
+
+		} // end for(String playerInfoKey : StartUpPhaseModel.playerInfo.keySet()
+
 		Random randomKey = new Random();
-	    
-		//loop until all reinforcement armies have been assigned
-		while(reinforcementArmies != 0){
-			
-			
-			//choose strongest territory to put armies into
-			String randomWeakestKey = weakestTerritoryList.get(randomKey.nextInt(weakestTerritoryList.size()) );
-			
+
+		// loop until all reinforcement armies have been assigned
+		while (reinforcementArmies != 0) {
+
+			// choose strongest territory to put armies into
+			String randomWeakestKey = weakestTerritoryList.get(randomKey.nextInt(weakestTerritoryList.size()));
+
 			int playerInfoValue = StartUpPhaseModel.playerInfo.get(randomWeakestKey);
-			
-			//playerInfoValue = playerInfoValue + 1;
-			
+
+			// playerInfoValue = playerInfoValue + 1;
+
 			playerInfoValue = playerInfoValue + reinforcementArmies;
-			
+
 			StartUpPhaseModel.playerInfo.put(randomWeakestKey, playerInfoValue);
-			
+
 			String[] playerKeySplit = randomWeakestKey.split("-");
-			//reinforcementArmies--;
-			
+			// reinforcementArmies--;
+
 			reinforcementArmies = 0;
-			
+
 			obj.setTotalReinforcementArmies(reinforcementArmies);
 			obj.setLatestArmies(playerInfoValue);
-			
+
 			setChanged();
 			obj.setMsgUI("placementView," + playerKeySplit[0] + "," + playerKeySplit[1]);
 			notifyObservers(this);
-			
+
 		}
-		
+
 	}
-	
-	
 
 	@Override
 	public void fortify(int player) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void attack(String player, HashMap<String, List<String>> territoryMap) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("--------------------------");
+		System.out.println("ATTACK PHASE UNINITIATED DUE TO ADOPTION OF BENEVOLENT STRATEGY ");
+		System.out.println("--------------------------");
+
 	}
 
 }
