594063d717b0cc1d90ce8ecb1d69022dd14c188e
drvg5 <drvg5@DESKTOP-2HQ1VCH.wireless.concordia.ca>
2017-11-30 00:20:15 -0500

    Cheater strategy implemented

--- RiskComputerGame/src/com/risk/behavior/CheaterBehaviorImpl.java
+++ RiskComputerGame/src/com/risk/behavior/CheaterBehaviorImpl.java
@@ -6,10 +6,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import com.risk.model.AttackPhaseModel;
-import com.risk.model.FortificationPhaseModel;
 import com.risk.model.ReinforcementPhaseModel;
 import com.risk.model.StartUpPhaseModel;
-import com.risk.ui.FortificationUI;
 import com.risk.ui.ReinforcementsUI;
 
 public class CheaterBehaviorImpl implements PlayerBehavior {
@@ -26,31 +24,61 @@ public class CheaterBehaviorImpl implements PlayerBehavior {
 
 	@Override
 	public void fortify(String player) {
-
-
-		FortificationPhaseModel obj = new FortificationPhaseModel();
-		FortificationUI objUI = new FortificationUI();
-		obj.addObserver(objUI);
-		obj.cheaterFortification(player);
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void attack(String player, HashMap<String, List<String>> territoryMap) {
 
-		for (Entry<String, Integer> toPrint : StartUpPhaseModel.playerInfo.entrySet()) {
-			System.out.println("before cheating");
+		System.out.println("---------------------------------------------------------------------------");
+		System.out.println("****************** ATTACK PHASE FOR PLAYER " + player + " BEGINS ***********************");
+		System.out.println("---------------------------------------------------------------------------");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------------");
 
+		System.out.println("CHEATER STRATEGY TO BE IMPLEMENTED");
+		System.out.println("---------------------------------------------------------------------------");
+
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------------");
+
+		System.out.println("PLAYER " + player + " STATS BEFORE CHEATING");
+		System.out.println("---------------------------------------------------------------------------");
+
+		System.out.println();
+		System.out.println();
+
+		for (Entry<String, Integer> toPrint : StartUpPhaseModel.playerInfo.entrySet()) {
+			System.out.println("---------------------------------------------------------------------------");
 			System.out.println(toPrint.getKey() + "\t" + "\t" + toPrint.getValue());
+			System.out.println("---------------------------------------------------------------------------");
+
 		}
 
 		// method to find adjacent countries and conquer them all
 		AttackPhaseModel.conquerAllAdjacentCountries(player, territoryMap);
 
-		for (Entry<String, Integer> toPrint : StartUpPhaseModel.playerInfo.entrySet()) {
-			System.out.println("after cheating");
+		System.out.println("---------------------------------------------------------------------------");
 
+		System.out.println("CHEATER STRATEGY IMPLEMENTED");
+
+		System.out.println("---------------------------------------------------------------------------");
+		System.out.println();
+
+		System.out.println("---------------------------------------------------------------------------");
+		System.out.println("PLAYER " + player + " STATS AFTER CHEATING");
+		System.out.println("---------------------------------------------------------------------------");
+
+		System.out.println();
+		System.out.println();
+
+		for (Entry<String, Integer> toPrint : StartUpPhaseModel.playerInfo.entrySet()) {
+			System.out.println("---------------------------------------------------------------------------");
 			System.out.println(toPrint.getKey() + "\t" + "\t" + toPrint.getValue());
+			System.out.println("---------------------------------------------------------------------------");
+
 		}
 	}
 
