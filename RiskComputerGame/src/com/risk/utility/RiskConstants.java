ec0e8a686b1a516e55b0132012c6171e4dc42053
nbhamrah <navjotkaurbhamrah@gmail.com>
2017-11-30 03:09:20 -0500

    RiskConstants class modified.

--- RiskComputerGame/src/com/risk/utility/RiskConstants.java
+++ RiskComputerGame/src/com/risk/utility/RiskConstants.java
@@ -1,16 +1,27 @@
 package com.risk.utility;
 
+
+/**
+ * The Class RiskConstants contains the constants to be used in the RISK game play.
+ */
 public class RiskConstants {
 	
 	
+	/** The Constant FIRST_EXG_ARMIES stores the armies a player gets on exchanging
+	 * the cards the first time. 
+	 */
 	public static final int FIRST_EXG_ARMIES = 5;
 	
+	/** The Constant CARD_1 stores the "Artillery" card name.*/
 	public static final String CARD_1 = "Artillery";
 	
+	/** The Constant CARD_2 stores the "Cavalry" card name. */
 	public static final String CARD_2 = "Cavalry";
 	
+	/** The Constant CARD_3 stores the "Infantry" card name. */
 	public static final String CARD_3 = "Infantry";
 	
+	/** The Constant String array CARD_TYPES stores the different type of cards. */
 	public static final String[] CARD_TYPES = {CARD_1,CARD_2,CARD_3}; 
 	
 
