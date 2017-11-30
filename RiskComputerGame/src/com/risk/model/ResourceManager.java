967b9c7318cdacb0dcf22409b87fb3ea5d529085
nbhamrah <navjotkaurbhamrah@gmail.com>
2017-11-30 04:15:28 -0500

    ResourceManager modified.

--- RiskComputerGame/src/com/risk/model/ResourceManager.java
+++ RiskComputerGame/src/com/risk/model/ResourceManager.java
@@ -6,14 +6,33 @@ import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
+/**
+ * The Class ResourceManager contains methods for saving data at particular state and resume again later.
+ * reading the saved files to load any previously saved game.
+ */
 public class ResourceManager {
 
+	/**
+	 * This method save the required data into a file to save the game at certain state.
+	 *
+	 * @param data the data
+	 * @param fileName the filename
+	 * @throws Exception the exception
+	 */
 	public static void save(Serializable data, String fileName) throws Exception {
 		try (ObjectOutputStream objOutput = new ObjectOutputStream(Files.newOutputStream(Paths.get(fileName)))) {
 			objOutput.writeObject(data);
 		}
 	}
 
+	/**
+	 * This method reads the saved data files to load the game saved previously.
+	 *
+	 * @param fileName the filename
+	 * @return the object
+	 * @throws Exception the exception
+	 */
 	public static Object load(String fileName) throws Exception {
 
 		try (ObjectInputStream objInput = new ObjectInputStream(Files.newInputStream((Paths.get(fileName))))) {
--- RiskComputerGame/src/com/risk/model/SaveAndLoadGame.java
+++ RiskComputerGame/src/com/risk/model/SaveAndLoadGame.java
@@ -6,25 +6,63 @@ import java.util.List;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+
+/**
+ * The Class SaveAndLoadGame contains the variables used to store the data and load the data at a certain instant in the game.
+ */
 public class SaveAndLoadGame implements java.io.Serializable {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/** The main map to save. */
 	public HashMap<String, List<String>> mainMapToSave;
+	
+	/** The domination old to save. */
 	public HashMap<String, String> dominationOldToSave = new HashMap<String, String>();
+	
+	/** The domination new to save. */
 	public HashMap<String, String> dominationNewToSave = new HashMap<String, String>();
+	
+	/** The player info to save. */
 	public HashMap<String, Integer> playerInfoToSave = new HashMap<String, Integer>();
+	
+	/** The terr per player to save. */
 	public HashMap<String, Integer> terrPerPlayerToSave = new HashMap<String, Integer>();
+	
+	/** The terr cont to save. */
 	public HashMap<String, String> terrContToSave = new HashMap<String, String>();
+	
+	/** The terr per cont to save. */
 	public TreeMap<String, Integer> terrPerContToSave = new TreeMap<String, Integer>();
+	
+	/** The reinforcement to save. */
 	public HashMap<String, Integer> reinforcementToSave = new HashMap<String, Integer>();
+	
+	/** The player cards to save. */
 	public HashMap<String, ArrayList<String>> playerCardsToSave = new HashMap<String, ArrayList<String>>();
+	
+	/** The prev player cards to save. */
 	public HashMap<String, ArrayList<String>> prevPlayerCardsToSave = new HashMap<String, ArrayList<String>>();
+	
+	/** The strategies to save. */
 	public HashMap<Integer, String> strategiesToSave = new HashMap<Integer, String>();
+	
+	/** The continent control value hash map to save. */
 	public HashMap<String, Integer> continentControlValueHashMapToSave = new HashMap<String, Integer>();
+	
+	/** The number of players to save. */
 	public int numberOfPlayersToSave;
+	
+	/** The current plyr strategy to save. */
 	public String currentPlyrStrategyToSave;
+	
+	/** The state. */
 	public String state;
+	
+	/** The country taken to save. */
 	public TreeSet<String> countryTakenToSave = new TreeSet<String>();
+	
+	/** The total terr to save. */
 	public  int totalTerrToSave = 0;
 }
